72ff7ed983129af5b407a27390a9ff15ebb72816
Lee JaeHyun <myleje@gmail.com>
2024-07-29 17:22:42 +0900

    .

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // go to, goes to
+    // 람다식 기호 발음 : go to, goes to
     private float h => Input.GetAxis("Horizontal");
     private float v => Input.GetAxis("Vertical");
     private float r => Input.GetAxis("Mouse X");
@@ -16,5 +16,6 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
         transform.Translate(moveDir.normalized * Time.deltaTime * 6.0f);
+        transform.Rotate(Vector3.up * Time.deltaTime * r * 200.0f);
     }
 }
