5e4964ea3ae9778dcb23508e485ca3a0e655e8ff
Lee JaeHyun <myleje@gmail.com>
2024-07-29 17:19:28 +0900

    .

---
+++ Assets/02_Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // go to, goes to
+    private float h => Input.GetAxis("Horizontal");
+    private float v => Input.GetAxis("Vertical");
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
