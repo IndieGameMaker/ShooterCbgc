c28f1054355be073fad301d4108f722608df42f9
Lee JaeHyun <myleje@gmail.com>
2024-07-29 16:36:59 +0900

    .

--- Assets/02_Scripts/PhotonManager.cs
+++ Assets/02_Scripts/PhotonManager.cs
@@ -18,4 +18,10 @@ public class PhotonManager : MonoBehaviourPunCallbacks
         // 포톤 서버 접속
         PhotonNetwork.ConnectUsingSettings();
     }
+
+    // 포톤 서버에 접속 했을 때 호출되는 콜백
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("포톤 서버에 접속 완료");
+    }
 }
