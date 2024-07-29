1ebccc119645c7face86a284c09324b2318c25fb
Lee JaeHyun <myleje@gmail.com>
2024-07-29 17:21:25 +0900

    .

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     // go to, goes to
     private float h => Input.GetAxis("Horizontal");
     private float v => Input.GetAxis("Vertical");
+    private float r => Input.GetAxis("Mouse X");
 
     void Start()
     {
@@ -13,6 +14,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        transform.Translate(moveDir.normalized * Time.deltaTime * 6.0f);
     }
 }
