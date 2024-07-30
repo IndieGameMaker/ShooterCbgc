ae7591e68a6da9a0a2059bcf5673cf1b5a4eff5c
Lee JaeHyun <myleje@gmail.com>
2024-07-30 14:18:05 +0900

    .

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     private float h => Input.GetAxis("Horizontal");
     private float v => Input.GetAxis("Vertical");
     private float r => Input.GetAxis("Mouse X");
+    private bool isFire => Input.GetMouseButtonDown(0);
 
     private PhotonView pv;
 
@@ -23,8 +24,15 @@ public class PlayerController : MonoBehaviour
     {
         if (pv.IsMine == false) return;
 
+        // 이동 및 회전 처리
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
         transform.Translate(moveDir.normalized * Time.deltaTime * 6.0f);
         transform.Rotate(Vector3.up * Time.deltaTime * r * 200.0f);
+
+        // 발사로직
+        if (isFire)
+        {
+
+        }
     }
 }
